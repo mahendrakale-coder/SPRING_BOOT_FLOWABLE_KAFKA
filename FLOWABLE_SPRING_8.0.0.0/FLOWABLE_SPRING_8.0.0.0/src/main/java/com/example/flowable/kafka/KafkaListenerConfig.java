d2600850f91306e31f269dab64db24d425b5e884
mahendrakale-coder <mahendra.kale@ebixcash.com>
2026-05-04 17:20:13 +0530

    Delete FLOWABLE_SPRING_8.0.0.0/FLOWABLE_SPRING_8.0.0.0 directory

--- FLOWABLE_SPRING_8.0.0.0/FLOWABLE_SPRING_8.0.0.0/src/main/java/com/example/flowable/kafka/KafkaListenerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.flowable.kafka;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Configuration
-@EnableKafka
-public class KafkaListenerConfig {
-}
