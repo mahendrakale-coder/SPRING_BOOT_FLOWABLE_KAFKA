626a93de3f9d8c7e79b1a7dbf40916931f715366
mahendrakale-coder <mahendra.kale@ebixcash.com>
2026-05-04 17:22:01 +0530

    Add files via upload

--- FLOWABLE_SPRING_8.0.0.0/src/main/java/com/example/flowable/FlowableApp.java
+++ FLOWABLE_SPRING_8.0.0.0/src/main/java/com/example/flowable/FlowableApp.java
@@ -2,7 +2,9 @@ package com.example.flowable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication(scanBasePackages = "com.example.flowable")
 public class FlowableApp {
     public static void main(String[] args) {
